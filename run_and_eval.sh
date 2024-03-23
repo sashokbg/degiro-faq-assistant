50808a378af976d961d3fe5272eb4873ee24f4a4
Alexander KIRILOV <alexander.n.kirilov@gmail.com>
2024-03-23 12:25:06 +0100

    added more test data and chat gpt variant

--- run_and_eval.sh
+++ run_and_eval.sh
@@ -1,11 +1,30 @@
 #pf connection create -f openai_connection.yaml
 
+variant=''
+
+if [ -n $1 ]; then
+  variant=$1
+fi
+
 pf run delete -n first_run -y
 pf run delete -n first_run_eval -y
 
-pf run create --flow . --data batch_data.jsonl --stream --name first_run --column-mapping question='${data.question}'
+if [ -z $variant ]; then
+  echo "Running default variant"
+  pf run create --flow . --data batch_data.jsonl --stream --name first_run --column-mapping question='${data.question}'
+else
+  echo "Running $variant"
+  pf run create --flow . --data batch_data.jsonl --stream --name first_run --variant="\${chat.$variant}" --column-mapping question='${data.question}'
+fi
 
-pf run create --flow evaluation --data batch_data.jsonl --column-mapping groundtruth_link='${data.link}' --column-mapping question='${data.question}' answer='${run.outputs.answer}' --run first_run --stream --name first_run_eval
+pf run create --flow evaluation \
+  --data batch_data.jsonl \
+  --column-mapping groundtruth_link='${data.link}' \
+  --column-mapping question='${data.question}' \
+  answer='${run.outputs.answer}' \
+  --run first_run \
+  --stream \
+  --name first_run_eval
 
 pf run show-metrics -n first_run_eval
 
